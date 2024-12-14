b6b1834645628733a7abeedc83d0f47d4022673e
Schuldiner92 <jhon.sanchezj92@gmail.com>
2024-12-13 19:23:45 -0500

    async - await

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -6,10 +6,24 @@ import {heroes} from '../data/heroes';
  */
 export const asyncComponent = (element) =>{
 
+    const id1 = '5d86371f97c29d020f1e1f6dz';
     
-
+    findHero(id1)
+        .then(name => element.innerHTML = name)
+        .catch(error => element.innerHTML = error)
+    
 }
 
-const findHero = () => {
+/**
+ * 
+ * @param {String} id 
+ * @returns  {Promise<String>}
+ */
+const findHero = async(id) => {
     
+    const hero = heroes.find(hero => hero.id === id);
+    if(!hero)
+        throw `Hero with id ${id} not found`;
+    return hero.name;
+
 }
